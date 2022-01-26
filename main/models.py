34a6e38be70944a6cc2d26d1bc0ce512cf3b2ff4
marinaRomanchuk <marina.romanchuk23@gmail.com>
2022-01-26 16:42:24 +0300

    fixed models

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=50)
-    nickname = models.CharField(max_length=30, unique=True)
-    personal_information = models.TextField(max_length=300)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=300)
+    date_of_birth = models.DateField()
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
     profile_photo = models.ImageField(null=True, blank=True)
 
     class Meta:
@@ -13,39 +14,41 @@ class User(models.Model):
         verbose_name_plural = 'Users'
 
     def __str__(self):
-        return f'{self.nickname}'
+        return self.user.username
 
 
 class Post(models.Model):
     photo = models.ImageField()
     description = models.TextField(max_length=500, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
     def __str__(self):
-        return f'{self.description}'
+        return self.timestamp
 
 
 class Comment(models.Model):
-    comment_text = models.TextField(max_length=500)
+    text = models.TextField(max_length=500)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         verbose_name = 'Comment'
         verbose_name_plural = 'Comments'
 
     def __str__(self):
-        return f'{self.comment_text}'
+        return self.text
 
 
 class Like(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         verbose_name = 'Like'
@@ -54,7 +57,8 @@ class Like(models.Model):
 
 class Dislike(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         verbose_name = 'Dislike'
@@ -62,5 +66,6 @@ class Dislike(models.Model):
 
 
 class Followers(models.Model):
-    follower = models.ForeignKey('User', on_delete=models.CASCADE)
-    following = models.ForeignKey('User', on_delete=models.CASCADE)
+    follower = models.ForeignKey('UserProfile', related_name='follower', on_delete=models.CASCADE)
+    following = models.ForeignKey('UserProfile', related_name='following', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
