18bb2ed83b6b5ded107c54112467cc98882bd480
marinaRomanchuk <marina.romanchuk23@gmail.com>
2022-01-28 15:41:07 +0300

    setting postgresql

--- instagram_own_version/settings.py
+++ instagram_own_version/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'instagram_own_version.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'instagram_project',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
