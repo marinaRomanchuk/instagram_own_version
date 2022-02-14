ef7d913615d17a66adac112a54cf2d010f0f6f1e
Marina <marina.romanchuk23@gmail.com>
2022-02-14 12:44:02 +0300

    adding one more query parameter for user_id

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from instagram_own_version.permissions import IsAuthor
 from posts.models import Post
 from posts.serializers import PostSerializer
-from users.models import Followers
+from users.models import Followers, User
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -14,13 +14,16 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def list(self, request):
         queryset = Post.objects.all()
+        user_parameter = int(self.request.GET.get("user_id", None))
         feed_parameter = self.request.GET.get("feed", None)
-        if feed_parameter:
-            fol = Followers.objects.filter(follower=request.user).values_list(
-                "following", flat=True
-            )
-            queryset = queryset.filter(user__in=fol)
-        else:
-            queryset = queryset.filter(user=request.user)
+        if user_parameter:
+            user_obj = User.objects.get(id=user_parameter)
+            if feed_parameter:
+                fol = Followers.objects.filter(follower=user_obj).values_list(
+                    "following", flat=True
+                )
+                queryset = queryset.filter(user__in=fol)
+            else:
+                queryset = queryset.filter(user=user_obj)
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
