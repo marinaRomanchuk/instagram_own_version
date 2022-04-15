0b2c2e3cef92959e658cf3437997de69e91d033c
Marina <marina.romanchuk23@gmail.com>
2022-04-15 12:55:45 +0300

    adding ordering

--- posts/models.py
+++ posts/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
     class Meta:
         verbose_name = "Post"
         verbose_name_plural = "Posts"
+        ordering = ["-timestamp"]
 
     def __str__(self):
         return f"{self.user.username}'s post"
